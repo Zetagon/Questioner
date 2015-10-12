8c1cc4c961a4048474c74461dd84f526a215c2ee
Zetagon <leo.ericson@yahoo.se>
2015-10-12 15:14:23 +0200

    load info
    Created and completed the method load_info(name) in class  Questioner.
    
    also created a test file for testing the method.

--- lib/Questioner.rb
+++ lib/Questioner.rb
@@ -3,8 +3,9 @@ class Questioner
 	
 	end
 	
-	def load_glossary()
-	
+ 
+	def load_glossary(name)
+		
 	end
 	
 	def new_question()
@@ -14,5 +15,18 @@ class Questioner
 	def load_question()
 	
 	end
+	#Loads a file named info
+	#info contains some info about the glossaries/questions in the other files in the 
+	#same directory
+	#returns the text in the file, in the form of an array.	
+	def load_info(name)
+		ary = []
+		Dir.chdir("../data/#{name}"){
+		File.open("info.txt", mode = "r"){|f| f.each do |x| ary.push(x)end}
+		}
+		return ary
+	end
 	
 end
+q = Questioner.new()
+puts q.load_glossary("test")
